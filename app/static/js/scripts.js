a447e9e9c8da08181de45932bc1b35ede84b6d93
Dierlen Bandeira <dierlen84@gmail.com>
2024-10-15 00:22:14 -0300

    arrumei a tela de home

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -6,12 +6,17 @@ document.getElementById("upButton").addEventListener("click", function() {
     setTimeout(function() {
         const descriptionElement = document.getElementById("description");
         const startButton = document.getElementById("startButton");
+        const backgroundElement = document.querySelector('.background'); // Seleciona o fundo
+
         
         // Exibe a descrição alterando diretamente a opacidade
         descriptionElement.style.opacity = "1";
         
         // Exibe o botão Iniciar alterando diretamente a opacidade
         startButton.style.opacity = "1";
+
+        // Diminuir o brilho do fundo da tela
+        backgroundElement.style.filter = "brightness(0.2)";
 
     }, 1000); // Tempo para a animação do botão de seta terminar
 });
